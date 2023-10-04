8795fad8c8fb8916a426d396505f11f0bf08150b
Fathul <basyair.fathul@gmail.com>
2023-10-04 18:26:36 +0700

    add program_esp32_v1.5-rev3

---
+++ program_server_1/program_platformio/program_esp32_v1.5-rev3/src/10_serialData.cpp
@@ -0,0 +1,52 @@
+/*! @file 10_serialData.cpp
+ * @version 1.5-rev3
+*/
+
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include "1_configProgram.h"
+
+int callback_1, callback_4;
+void sendDataESP(void) {
+    // buat object DynamicJsonDocument data
+    DynamicJsonDocument data(500);
+    // ambil command callback
+    deserializeJson(data, atmegaSerial);
+    callback_1 = data["callback_1"];
+    if(callback_1 == 1) {
+        // masukan nilai ke dalam format json
+        // jika suhu pada MCU tidak terdeteksi, maka nilai FIS Heater 100
+        if(datadht.temperature == 0.0 || datadht.temperature <= 0.0 || stateFuzzy == false)
+        {
+            data["heater"] = 100;
+            data["fan"] = 0;
+            data["callback_2"] = 1;
+            data["callback_3"] = 1;
+        } else {
+            if(datadht.temperature >= 50.01 || datadht.humidity <= 30.0) 
+            {
+                data["heater"] = 60;
+                data["fan"] = 0;
+            } else {
+                data["heater"] = datafuzzy.fisOutputHeater;
+                data["fan"] = datafuzzy.fisOutputFan;
+            }
+            data["callback_2"] = 1;
+            data["callback_3"] = 1;
+        }
+        // kirim data ke serial board atmega
+        serializeJson(data, atmegaSerial);
+    }
+}
+
+void getDataAtmega(void) {
+    DynamicJsonDocument data(500);
+    DeserializationError err = deserializeJson(data, atmegaSerial);
+    if(!err) {
+        callback_4 = data["callback_4"];
+        if(callback_4 == 1){
+            datafuzzy.powerHeater = data["dimheater"];
+            datafuzzy.powerFan = data["dimfan"];
+        }
+    }
+}
---
+++ program_server_1/program_platformio/program_esp32_v1.5-rev3/src/main.cpp
@@ -0,0 +1,119 @@
+/*! @file main.cpp
+ * @version 1.5-rev3
+ * @mainpage
+ * Program Pengambilan dan Mengirim data ESP32 ke server Raspberry Pi (Web Server Local) (PlatformIO)
+ * @section
+ * VERSION : 1.5-rev3
+ * NAMA    : FATHUL BASYAIR
+ * NPM     : 1904105010004
+ * PRODI   : TEKNIK ELEKTRO (BIDANG TEKNIK TENAGA LISTRIK)
+ */
+
+#include <Arduino.h>
+#include <DHT.h>
+#include <Wire.h>
+#include <WiFi.h>
+#include <EEPROM.h>
+#include <WebServer.h>
+#include <PZEM004Tv30.h>
+#include <SoftwareSerial.h>
+#include "1_configProgram.h"
+
+// Insert ssid and password server
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+// TCP server at port 80 will respond to HTTP requests
+WebServer server(80);
+String page;
+
+// Fuzzy Logic Object
+DATAFUZZY datafuzzy;
+FIS_TYPE g_fisInput[fis_gcI];
+FIS_TYPE g_fisOutput[fis_gcO];
+
+// stateFuzzy enable or disable
+// default : true (enable)
+bool stateFuzzy = true;
+
+// BuzzerState Main
+int BuzzerState = HIGH;
+bool buzzerSwitch = true;
+
+// Sensor PZEM-004T Object
+#if defined(ESP32)
+PZEM004Tv30 pzem(Serial2, RXD2, TXD2);
+#else
+PZEM004Tv30 pzem(Serial2);
+#endif
+DATAPZEM datapzem;
+
+// Sensor DHT Object
+#define DHTTYPE   DHT21
+DHT dht(pinDHT, DHTTYPE);
+DATADHT datadht;
+
+// Serial Data Arduino Object
+SoftwareSerial atmegaSerial(atmegaRX1, atmegaTX1);
+
+// create a program 1 for WebServer
+void program_1(void* parameter) {
+  // Initialize DHT-22 Sensor
+  dht.begin();
+  // run repeatedly funcMain() if program fuzzy ready
+  while(true) {
+    funcMain();
+  }
+}
+
+// create a program 2 for WebServer
+void program_2(void* parameter) {
+  // Initialize ESP-32 WiFi Server
+  initWiFi();
+  Serial.print(F("ESP32 NEW HOSTNAME : "));
+  Serial.println(WiFi.getHostname());
+  server_setup();
+  // run repeatedly server.handleClient() if WiFi connected
+  while(true) {
+    server.handleClient();
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  // Initialize serial communication with a baudrate of 9600 bits per seconds (bps)
+  EEPROM.begin(EEPROM_SIZE);
+  Serial.begin(9600);
+  atmegaSerial.begin(9600);
+  
+  // Check fuzzy condition is disabled or not
+  int stateFuzzySwitch = EEPROM.read(0);
+  int stateBuzzerSwitch = EEPROM.read(1);
+  if(stateFuzzySwitch == 0) stateFuzzy = false;
+  if(stateBuzzerSwitch == 0) buzzerSwitch = false;
+
+  // Turn on buzzer_startup if server esp is ready!
+  buzzer_startup(pin_buzzer);
+  delay(5000);
+
+  // Install the new ESP32 WiFi configuration
+  // check reconnectWiFi if esp32 wifi is disconnected
+  WiFi.onEvent(WiFiStationConnected, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_CONNECTED);
+  WiFi.onEvent(WiFiGotIP, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_GOT_IP);
+  WiFi.onEvent(WiFiStationDisconnected, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
+
+  // create a Task for program 1
+  xTaskCreatePinnedToCore(
+    program_1, "programMain", 5000, NULL, 1, NULL, ARDUINO_RUNNING_CORE
+  );
+  
+  // create a Task for program 2
+  xTaskCreatePinnedToCore(
+    program_2, "programWebServer", 5000, NULL, 2, NULL, ARDUINO_RUNNING_CORE
+  );
+  
+}
+
+void loop() {
+  // NOTHING  
+}
